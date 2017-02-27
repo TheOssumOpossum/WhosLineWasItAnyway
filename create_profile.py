fb8c80177743e13e0329aa6c67f219af476931a3
TheOssumOpossum <v.bommier@gmail.com>
2017-02-26 22:07:56 -0600

    json formatting for bing api function calls

---
+++ create_profile.py
@@ -0,0 +1,30 @@
+########### Python 2.7 #############
+import httplib, urllib, base64, json
+from bing_key import *
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    "locale":"en-US",
+    'Ocp-Apim-Subscription-Key': BING_KEY_SPEAKER,
+    'locale':"en-us",
+}
+
+params = urllib.urlencode({
+    'locale':'en-us',
+})
+
+
+
+try:
+    body = json.dumps({'locale': 'en-us'})
+    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/spid/v1.0/identificationProfiles?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
